cc8419e0b1935267586e499c5c1d88cc500432b2
mherring2 <mherring2@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2012-09-24 21:11:40 +0000

    switch off pipes & cables
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/renderer@1010 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- searender/rules.c
+++ searender/rules.c
@@ -577,8 +577,8 @@ object_rules(cables) {
 rules {
   
   type("shoreline_construction") object(shoreline);
-  type("pipeline_submarine") object(pipelines);
-  type("cable_submarine") object(cables);
+//  type("pipeline_submarine") object(pipelines);
+//  type("cable_submarine") object(cables);
   type("separation_zone") object(separation);
   type("separation_crossing") object(separation);
   type("separation_roundabout") object(separation);
