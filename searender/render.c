ae476c8de695da72a1dc32b4410b395f6f957d86
mherring2 <mherring2@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2013-01-16 05:50:00 +0000

    update
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/renderer@1104 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- searender/render.c
+++ searender/render.c
@@ -1088,210 +1088,211 @@ char *charString(Item_t *item, char *type, int idx) {
       }
       break;
     case LIGHTS:
-      {
-        int secmax = countObjects(item, "light");
-        if ((idx == 0) && (secmax > 0)) {
-          struct SECT {
-            struct SECT *next;
-            int dir;
-            LitCHR_t chr;
-            ColCOL_t col;
-            ColCOL_t alt;
-            char *grp;
-            double per;
-            double rng;
-          } *lights = NULL;
-          for (int i = secmax; i > 0; i--) {
-            struct SECT *tmp = calloc(1, sizeof(struct SECT));
-            tmp->next = lights;
-            lights = tmp;
-            obj = getObj(item, LIGHTS, i);
-            if ((att = getAtt(obj, CATLIT)) != NULL) {
-              lights->dir = testAtt(att, LIT_DIR);
-            }
-            if ((att = getAtt(obj, LITCHR)) != NULL) {
-              lights->chr = att->val.val.e;
-              switch (lights->chr) {
-                case CHR_AL:
-                  lights->chr = CHR_F;
-                  break;
-                case CHR_ALOC:
-                  lights->chr = CHR_OC;
-                  break;
-                case CHR_ALLFL:
-                  lights->chr = CHR_LFL;
-                  break;
-                case CHR_ALFL:
-                  lights->chr = CHR_FL;
-                  break;
-                case CHR_ALFFL:
-                  lights->chr = CHR_FFL;
-                  break;
-                default:
-                  break;
-              }
-            }
-            if ((att = getAtt(obj, SIGGRP)) != NULL) {
-              lights->grp = att->val.val.a;
-            } else {
-              lights->grp = "";
-            }
-            if ((att = getAtt(obj, SIGPER)) != NULL) {
-              lights->per = att->val.val.f;
-            }
-            if ((att = getAtt(obj, VALNMR)) != NULL) {
-              lights->rng = att->val.val.f;
-            }
-            if ((att = getAtt(obj, COLOUR)) != NULL) {
-              lights->col = att->val.val.l->val;
-              if (att->val.val.l->next != NULL)
-                lights->alt = att->val.val.l->next->val;
-            }
-          }
-          struct COLRNG {
-            int col;
-            double rng;
-          } colrng[14];
-          while (lights != NULL) {
-            strcpy(string2, "");
-            bzero(colrng, 14*sizeof(struct COLRNG));
-            colrng[lights->col].col = 1;
-            colrng[lights->col].rng = lights->rng;
-            struct SECT *this = lights;
-            struct SECT *next = lights->next;
-            while (next != NULL) {
-              if ((this->dir == next->dir) && (this->chr == next->chr) &&
-                  (strcmp(this->grp, next->grp) == 0) && (this->per == next->per)) {
-                colrng[next->col].col = 1;
-                if (next->rng > colrng[next->col].rng)
-                  colrng[next->col].rng = next->rng;
-                struct SECT *tmp = lights;
-                while (tmp->next != next) tmp = tmp->next;
-                tmp->next = next->next;
-                free(next);
-                next = tmp->next;
-              } else {
-                next = next->next;
-              }
-            }
-            if (this->chr != CHR_UNKN) {
-              if (this->dir) strcpy(string2, "Dir.");
-              strcat(string2, light_characters[this->chr]);
-              if (strcmp(this->grp, "") != 0) {
-                if (this->grp[0] == '(')
-                  sprintf(strchr(string2, 0), "%s", this->grp);
-                else
-                  sprintf(strchr(string2, 0), "(%s)", this->grp);
-              } else {
-                if (strlen(string2) > 0) strcat(string2, ".");
-              }
-              int n = 0;
-              for (int i = 0; i < 14; i++) if (colrng[i].col) n++;
-              double max = 0.0;
-              for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng > max)) max = colrng[i].rng;
-              double min = max;
-              for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng > 0.0) && (colrng[i].rng < min)) min = colrng[i].rng;
-              if (min == max) {
-                for (int i = 0; i < 14; i++) if (colrng[i].col) strcat(string2, light_letters[i]);
-              } else {
-                for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng == max)) strcat(string2, light_letters[i]);
-                for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng < max) && (colrng[i].rng > min)) strcat(string2, light_letters[i]);
-                for (int i = 0; i < 14; i++) if (colrng[i].col && colrng[i].rng == min) strcat(string2, light_letters[i]);
-              }
-              strcat(string2, ".");
-              if (this->per > 0.0) sprintf(strchr(string2, 0), "%gs", this->per);
-              if (max > 0.0) {
-                sprintf(strchr(string2, 0), "%g", max);
-                if (min != max) {
-                  if (n == 2) strcat(string2, "/");
-                  else if (n > 2) strcat(string2, "-");
-                  if (min < max) sprintf(strchr(string2, 0), "%g", min);
-                }
-                strcat(string2, "M");
-              }
-              if (strlen(string1) > 0) strcat(string1, "\n");
-              strcat(string1, string2);
-            }
-            lights = this->next;
-            free(this);
-            this = lights;
-          }
-        } else {
+    {
+      int secmax = countObjects(item, "light");
+      if ((idx == 0) && (secmax > 0)) {
+        struct SECT {
+          struct SECT *next;
+          int dir;
+          LitCHR_t chr;
+          ColCOL_t col;
+          ColCOL_t alt;
+          char *grp;
+          double per;
+          double rng;
+        } *lights = NULL;
+        for (int i = secmax; i > 0; i--) {
+          struct SECT *tmp = calloc(1, sizeof(struct SECT));
+          tmp->next = lights;
+          lights = tmp;
+          obj = getObj(item, LIGHTS, i);
           if ((att = getAtt(obj, CATLIT)) != NULL) {
-            if (testAtt(att, LIT_DIR))
-              strcat(string1, "Dir");
+            lights->dir = testAtt(att, LIT_DIR);
           }
-          if ((att = getAtt(obj, MLTYLT)) != NULL)
-            sprintf(strchr(string1, 0), "%s", stringValue(att->val));
           if ((att = getAtt(obj, LITCHR)) != NULL) {
-            char *chrstr = strdup(stringValue(att->val));
-            Att_t *grp = getAtt(obj, SIGGRP);
-            if (grp != NULL) {
-              char *grpstr = strdup(stringValue(grp->val));
-              switch (att->val.val.e) {
-                case CHR_QLFL:
-                  sprintf(strchr(string1, 0), "Q(%s)+LFl", grpstr);
-                  break;
-                case CHR_VQLFL:
-                  sprintf(strchr(string1, 0), "VQ(%s)+LFl", grpstr);
-                  break;
-                case CHR_UQLFL:
-                  sprintf(strchr(string1, 0), "UQ(%s)+LFl", grpstr);
-                  break;
-                default:
-                  sprintf(strchr(string1, 0), "%s(%s)", chrstr, grpstr);
-                  break;
-              }
-              free(grpstr);
-            } else {
-              sprintf(strchr(string1, 0), "%s", chrstr);
+            lights->chr = att->val.val.e;
+            switch (lights->chr) {
+              case CHR_AL:
+                lights->chr = CHR_F;
+                break;
+              case CHR_ALOC:
+                lights->chr = CHR_OC;
+                break;
+              case CHR_ALLFL:
+                lights->chr = CHR_LFL;
+                break;
+              case CHR_ALFL:
+                lights->chr = CHR_FL;
+                break;
+              case CHR_ALFFL:
+                lights->chr = CHR_FFL;
+                break;
+              default:
+                break;
             }
-            free(chrstr);
+          }
+          if ((att = getAtt(obj, SIGGRP)) != NULL) {
+            lights->grp = att->val.val.a;
+          } else {
+            lights->grp = "";
+          }
+          if ((att = getAtt(obj, SIGPER)) != NULL) {
+            lights->per = att->val.val.f;
+          }
+          if ((att = getAtt(obj, VALNMR)) != NULL) {
+            lights->rng = att->val.val.f;
           }
           if ((att = getAtt(obj, COLOUR)) != NULL) {
-            int n = countValues(att);
-            if (!((n == 1) && (idx == 0) && (testAtt(att, COL_WHT)))) {
-              if ((strlen(string1) > 0) && ((string1[strlen(string1)-1] != ')'))) 
-                strcat(string1, ".");
-              Lst_t *lst = att->val.val.l;
-              while (lst != NULL) {
-                strcat(string1, light_letters[lst->val]);
-                lst = lst->next;
-              }
-            }
-          }
-          if ((idx == 0) && (att = getAtt(obj, CATLIT)) != NULL) {
-            if (testAtt(att, LIT_VERT))
-              strcat(string1, "(vert)");
-            if (testAtt(att, LIT_HORI))
-              strcat(string1, "(hor)");
-          }
-          if ((strlen(string1) > 0) &&
-              ((getAtt(obj, SIGPER) != NULL) ||
-               (getAtt(obj, HEIGHT) != NULL) ||
-               (getAtt(obj, VALMXR) != NULL)) &&
-              (string1[strlen(string1)-1] != ')'))
-            strcat(string1, ".");
-          if ((att = getAtt(obj, SIGPER)) != NULL)
-            sprintf(strchr(string1, 0), "%ss", stringValue(att->val));
-          if ((idx == 0) && (item->objs.obj != LITMIN)) {
-            if ((att = getAtt(obj, HEIGHT)) != NULL)
-              sprintf(strchr(string1, 0), "%sm", stringValue(att->val));
-            if ((att = getAtt(obj, VALNMR)) != NULL)
-              sprintf(strchr(string1, 0), "%sM", stringValue(att->val));
-          }
-          if ((idx == 0) && (att = getAtt(obj, CATLIT)) != NULL) {
-            if (testAtt(att, LIT_FRNT))
-              strcat(string1, "(Front)");
-            if (testAtt(att, LIT_REAR))
-              strcat(string1, "(Rear)");
-            if (testAtt(att, LIT_UPPR))
-              strcat(string1, "(Upper)");
-            if (testAtt(att, LIT_LOWR))
-              strcat(string1, "(Lower)");
+            lights->col = att->val.val.l->val;
+            if (att->val.val.l->next != NULL)
+              lights->alt = att->val.val.l->next->val;
           }
         }
+        struct COLRNG {
+          int col;
+          double rng;
+        } colrng[14];
+        while (lights != NULL) {
+          strcpy(string2, "");
+          bzero(colrng, 14*sizeof(struct COLRNG));
+          colrng[lights->col].col = 1;
+          colrng[lights->col].rng = lights->rng;
+          struct SECT *this = lights;
+          struct SECT *next = lights->next;
+          while (next != NULL) {
+            if ((this->dir == next->dir) && (this->chr == next->chr) &&
+                (strcmp(this->grp, next->grp) == 0) && (this->per == next->per)) {
+              colrng[next->col].col = 1;
+              if (next->rng > colrng[next->col].rng)
+                colrng[next->col].rng = next->rng;
+              struct SECT *tmp = lights;
+              while (tmp->next != next) tmp = tmp->next;
+              tmp->next = next->next;
+              free(next);
+              next = tmp->next;
+            } else {
+              next = next->next;
+            }
+          }
+          if (this->chr != CHR_UNKN) {
+            if (this->dir) strcpy(string2, "Dir.");
+            strcat(string2, light_characters[this->chr]);
+            if (strcmp(this->grp, "") != 0) {
+              if (this->grp[0] == '(')
+                sprintf(strchr(string2, 0), "%s", this->grp);
+              else
+                sprintf(strchr(string2, 0), "(%s)", this->grp);
+            } else {
+              if (strlen(string2) > 0) strcat(string2, ".");
+            }
+            int n = 0;
+            for (int i = 0; i < 14; i++) if (colrng[i].col) n++;
+            double max = 0.0;
+            for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng > max)) max = colrng[i].rng;
+            double min = max;
+            for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng > 0.0) && (colrng[i].rng < min)) min = colrng[i].rng;
+            if (min == max) {
+              for (int i = 0; i < 14; i++) if (colrng[i].col) strcat(string2, light_letters[i]);
+            } else {
+              for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng == max)) strcat(string2, light_letters[i]);
+              for (int i = 0; i < 14; i++) if (colrng[i].col && (colrng[i].rng < max) && (colrng[i].rng > min)) strcat(string2, light_letters[i]);
+              for (int i = 0; i < 14; i++) if (colrng[i].col && colrng[i].rng == min) strcat(string2, light_letters[i]);
+            }
+            strcat(string2, ".");
+            if (this->per > 0.0) sprintf(strchr(string2, 0), "%gs", this->per);
+            if (max > 0.0) {
+              sprintf(strchr(string2, 0), "%g", max);
+              if (min != max) {
+                if (n == 2) strcat(string2, "/");
+                else if (n > 2) strcat(string2, "-");
+                if (min < max) sprintf(strchr(string2, 0), "%g", min);
+              }
+              strcat(string2, "M");
+            }
+            if (strlen(string1) > 0) strcat(string1, "\n");
+            strcat(string1, string2);
+          }
+          lights = this->next;
+          free(this);
+          this = lights;
+        }
+      } else {
+        if ((att = getAtt(obj, CATLIT)) != NULL) {
+          if (testAtt(att, LIT_DIR))
+            strcat(string1, "Dir");
+        }
+        if ((att = getAtt(obj, MLTYLT)) != NULL)
+          sprintf(strchr(string1, 0), "%s", stringValue(att->val));
+        if ((att = getAtt(obj, LITCHR)) != NULL) {
+          char *chrstr = strdup(stringValue(att->val));
+          Att_t *grp = getAtt(obj, SIGGRP);
+          if (grp != NULL) {
+            char *strgrp = strdup(stringValue(grp->val));
+            char *grpstr = strtok(strgrp, "()");
+            switch (att->val.val.e) {
+              case CHR_QLFL:
+                sprintf(strchr(string1, 0), "Q(%s)+LFl", grpstr);
+                break;
+              case CHR_VQLFL:
+                sprintf(strchr(string1, 0), "VQ(%s)+LFl", grpstr);
+                break;
+              case CHR_UQLFL:
+                sprintf(strchr(string1, 0), "UQ(%s)+LFl", grpstr);
+                break;
+              default:
+                sprintf(strchr(string1, 0), "%s(%s)", chrstr, grpstr);
+                break;
+            }
+            free(strgrp);
+          } else {
+            sprintf(strchr(string1, 0), "%s", chrstr);
+          }
+          free(chrstr);
+        }
+        if ((att = getAtt(obj, COLOUR)) != NULL) {
+          int n = countValues(att);
+          if (!((n == 1) && (idx == 0) && (testAtt(att, COL_WHT)))) {
+            if ((strlen(string1) > 0) && ((string1[strlen(string1)-1] != ')')))
+              strcat(string1, ".");
+            Lst_t *lst = att->val.val.l;
+            while (lst != NULL) {
+              strcat(string1, light_letters[lst->val]);
+              lst = lst->next;
+            }
+          }
+        }
+        if ((idx == 0) && (att = getAtt(obj, CATLIT)) != NULL) {
+          if (testAtt(att, LIT_VERT))
+            strcat(string1, "(vert)");
+          if (testAtt(att, LIT_HORI))
+            strcat(string1, "(hor)");
+        }
+        if ((strlen(string1) > 0) &&
+            ((getAtt(obj, SIGPER) != NULL) ||
+             (getAtt(obj, HEIGHT) != NULL) ||
+             (getAtt(obj, VALMXR) != NULL)) &&
+            (string1[strlen(string1)-1] != ')'))
+          strcat(string1, ".");
+        if ((att = getAtt(obj, SIGPER)) != NULL)
+          sprintf(strchr(string1, 0), "%ss", stringValue(att->val));
+        if ((idx == 0) && (item->objs.obj != LITMIN)) {
+          if ((att = getAtt(obj, HEIGHT)) != NULL)
+            sprintf(strchr(string1, 0), "%sm", stringValue(att->val));
+          if ((att = getAtt(obj, VALNMR)) != NULL)
+            sprintf(strchr(string1, 0), "%sM", stringValue(att->val));
+        }
+        if ((idx == 0) && (att = getAtt(obj, CATLIT)) != NULL) {
+          if (testAtt(att, LIT_FRNT))
+            strcat(string1, "(Front)");
+          if (testAtt(att, LIT_REAR))
+            strcat(string1, "(Rear)");
+          if (testAtt(att, LIT_UPPR))
+            strcat(string1, "(Upper)");
+          if (testAtt(att, LIT_LOWR))
+            strcat(string1, "(Lower)");
+        }
       }
+    }
       break;
     default: break;
  }
